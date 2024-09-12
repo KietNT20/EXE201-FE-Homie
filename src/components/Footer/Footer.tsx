536a8edbf6cb0258cc9aeae7a5854b8845577bd6
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-09-13 00:26:35 +0700

    styled: footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,66 +1,102 @@
-import { SocialIcon } from "react-social-icons";
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="footer__content">
-          <figure className="footer__content-logo">
-            <img
-              src="/api/placeholder/150/50"
-              alt="Homie logo"
-              className="footer__img"
-            />
-          </figure>
-          <p className="footer__content-company">Công Ty TNHH Homie</p>
-          <p className="footer__content-email">email: support@homie.com</p>
-          <p className="footer__content-hotline">Hotline: 1900 0933</p>
+        <div className="footer__top">
+          <div className="footer__top-content">
+            <figure className="footer__thumb">
+              <img
+                src="/public/logo-footer.png"
+                alt="Homie logo"
+                className="footer__thumb-img"
+              />
+            </figure>
+            <div className="footer__info">
+              <p className="footer__info-company">Công Ty TNHH Homie</p>
+              <p className="footer__info-email">email: support@homie.com</p>
+              <p className="footer__info-hotline">Hotline: 1900 0933</p>
+            </div>
+          </div>
+          <div className="footer__top-company">
+            <h3 className="footer__title">Công ty</h3>
+            <ul className="footer__list">
+              <li>
+                <a href="#">Giới thiệu Homie</a>
+              </li>
+              <li>
+                <a href="#">
+                  Tuyển dụng <span className="footer-news">Mới</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">Chi nhánh</a>
+              </li>
+              <li>
+                <a href="#">Kinh nghiệm</a>
+              </li>
+              <li>
+                <a href="#">Khuyến mãi</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__top-socials">
+            {' '}
+            <h3 className="footer__title">Xã hội</h3>
+            <ul className="footer__list">
+              <li>
+                <a href="#" target="_blank">
+                  Tiktok
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank">
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank">
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__top-services">
+            {' '}
+            <h3 className="footer__title">Dịch vụ</h3>
+            <ul className="footer__list">
+              <li>
+                <a href="#">Giúp việc nhà theo giờ</a>
+              </li>
+              <li>
+                <a href="#">Chăm sóc người bệnh</a>
+              </li>
+              <li>
+                <a href="#">Giặt giũ</a>
+              </li>
+              <li>
+                <a href="#">Nấu ăn gia đình</a>
+              </li>
+              <li>
+                <a href="#">Chăm sóc trẻ em</a>
+              </li>
+              <li>
+                <a href="#">Chăm sóc thú cưng</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="footer__company">
-          <h3 className="footer__company-title">Công ty</h3>
-          <ul className="footer__company-list">
-            <li>Giới thiệu</li>
-            <li>Homie</li>
-            <li>
-              Tuyển dụng{" "}
-              <span className="bg-white text-navy-900 text-xs px-1 rounded">
-                Mới
-              </span>
-            </li>
-            <li>Chi nhánh</li>
-            <li>Kinh nghiệm</li>
-            <li>Khuyến mãi</li>
-          </ul>
-        </div>
-        <div className="footer__socials">
-          {" "}
-          <h3 className="footer__socials-title">Xã hội</h3>
-          <ul className="footer__socials-list">
-            <li>Tiktok</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-          </ul>
-        </div>
-        <div className="footer__services">
-          {" "}
-          <h3 className="footer__services-title">Dịch vụ</h3>
-          <ul className="footer__services-list">
-            <li>Giúp việc nhà theo giờ</li>
-            <li>Chăm sóc người bệnh</li>
-            <li>Giặt giũ</li>
-            <li>Nấu ăn gia đình</li>
-            <li>Chăm sóc trẻ em</li>
-            <li>Chăm sóc thú cưng</li>
-          </ul>
-        </div>
-        <div className="footer__linksocials">
-          <SocialIcon url="https://www.instagram.com/homie.vn" />
-          <SocialIcon url="https://www.tiktok.com/homie.vn" />
-          <SocialIcon url="https://www.youtube.com/homie.vn" />
-          <SocialIcon url="https://www.facebook.com/homie.vn" />
-        </div>
-        <div className="footer__copyright">
-          <p>© 2024 Homie. All rights reserved.</p>
+        <div className="footer__bottom">
+          <div className="footer__bottom-linksocials">
+            <SocialIcon url="https://www.instagram.com/homie.vn" />
+            <SocialIcon url="https://www.tiktok.com/homie.vn" />
+            <SocialIcon url="https://www.youtube.com/homie.vn" />
+            <SocialIcon url="https://www.facebook.com/homie.vn" />
+          </div>
+          <p className="footer__bottom-copyright">
+            © 2024 Homie. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
