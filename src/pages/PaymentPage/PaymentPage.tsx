7fabcb55f80b2c16cd360690261c300d54415b30
HoangTV48 <tranhoang12112003@gmail.com>
2024-09-25 12:49:44 +0700

    paymentpage

---
+++ src/pages/PaymentPage/PaymentPage.tsx
@@ -0,0 +1,79 @@
+import logo from "../../assets/img/images.png";
+import logoMB from "../../assets/img/unnamed.png";
+import logoCash from "../../assets/img/money-cash-wealth-payment-line-icon-illustration-logo-template-suitable-for-many-purposes-free-vector.jpg";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button"; // MUI Button component for better styling
+
+const LoginPage = () => {
+  return (
+    <div className="loginPage flex">
+      <div className="login-container flex">
+        {/* Payment Info Section */}
+        <div className="payment-info">
+          <h2>Thông tin thanh toán</h2>
+          <div className="input-group">
+            <h3>Họ và tên</h3>
+            <TextField fullWidth variant="outlined" placeholder="Nhập tên" />
+          </div>
+
+          <div className="input-group">
+            <h3>Email</h3>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Nhập email của bạn"
+            />
+          </div>
+
+          <div className="input-group">
+            <h3>Số điện thoại</h3>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Nhập số điện thoại"
+            />
+          </div>
+
+          <div className="input-group">
+            <h3>Dịch vụ</h3>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Dịch vụ bạn muốn"
+            />
+          </div>
+
+          <div className="input-group">
+            <h3>Voucher</h3>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Nhập voucher"
+            />
+          </div>
+          <div className="payment-button1">
+            <Button
+              variant="contained"
+              color="primary"
+              className="payment-button"
+            >
+              Thanh Toán
+            </Button>
+          </div>
+        </div>
+
+        {/* Payment Methods Section */}
+        <div className="payment-methods">
+          <h3>Phương thức thanh toán</h3>
+          <div className="logos">
+            <img src={logo} alt="logo" />
+            <img src={logoMB} alt="logoMB" />
+            <img src={logoCash} alt="logoCash" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import AboutPage from "@/pages/AboutPage/AboutPage";
 import HomePage from "@/pages/HomePage/HomePage";
 import LoginPage from "@/pages/LoginPage/LoginPage";
 import RegisterPage from "@/pages/RegisterPage/RegisterPage";
+import PaymentPage from "@/pages/PaymentPage/PaymentPage";
 import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
@@ -14,6 +15,7 @@ const AppRoutes = () => {
         <Route path={PATH.ABOUT} element={<AboutPage />} />
         <Route path={PATH.LOGIN} element={<LoginPage />} />
         <Route path={PATH.REGISTER} element={<RegisterPage />} />
+        <Route path={PATH.PAYMENT} element={<PaymentPage />} />
       </Route>
     </Routes>
   );
