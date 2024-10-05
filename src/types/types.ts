fed750bea4698ddd7f94176845e10751e56f4096
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-05 18:55:27 +0700

    feat: add yup resolver

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,13 @@ export interface TokenResponse {
   access_token?: string;
   refresh_token?: string;
 }
+export interface InputProps {
+  label?: string;
+  type?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  size?: "small" | "medium";
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  sx?: object;
+}
