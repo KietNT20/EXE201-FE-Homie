d58178fb25df7fbb49aef0b676912ce1f1001271
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-05 16:04:58 +0700

    config axios and react-query for handle api

---
+++ src/api/apiUrl.ts
@@ -0,0 +1,9 @@
+export const API = {
+  LOGIN: "/auth/login",
+  REFRESH_TOKEN: "/auth/refresh-token",
+  LOGOUT: "/auth/logout",
+  USER: "/users",
+  SERVICE: "/services",
+  CATEGORY: "/categories",
+  PAYMENT: "/payments",
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import App, { queryClient } from "./App.tsx";
 import "./index.css";
 import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
