904287f2d757bac4a2fe2917aaf6a97fbc7aafd3
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-19 13:16:09 +0700

    refactor: update scss dart

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import './assets/scss/style.scss';
 import AppRoutes from './routes/AppRoutes';
+
 export const queryClient = new QueryClient();
 
 function App() {
