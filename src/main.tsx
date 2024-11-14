fd630d3d23f2bbb585390954536097221ff5e3ae
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-11-14 22:30:12 +0700

    refactor: react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+          <BrowserRouter
+            future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
+          >
             <StyledEngineProvider injectFirst>
               <App />
             </StyledEngineProvider>
